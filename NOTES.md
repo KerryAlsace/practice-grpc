d7476d678cb88c7493fa216829934aa3d33410eb
KerryAlsace <kerrydougherty@gmail.com>
2017-03-02 15:16:14 -0500

    set up client file

--- NOTES.md
+++ NOTES.md
@@ -2,4 +2,8 @@
 2. Fill out routes/practice_app.proto
 3. Generate pb:
   `protoc -I routes/ routes/practice_app.proto --go_out=plugins=grpc:routes`
-4. 
+4. Add to .env:
+  PORT=:50051
+  ADDRESS=localhost:50051
+5. Set up server file
+6. Set up client file
--- client/main.go
+++ client/main.go
@@ -1,1 +1,43 @@
 package main
+
+import (
+	"context"
+	"log"
+	"os"
+
+	"google.golang.org/grpc"
+)
+
+const defaultNum = 2
+
+func main() {
+	// Set up connection to the server
+	conn, err := grpc.Dial(os.Getenv(ADDRESS), grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Did not connect: %v", err)
+	}
+
+	defer conn.Close()
+	c := pb.NewCalculatorClient(conn)
+
+	// Contact the server and print out its response
+	num := defaultNum
+	if len(os.Args) > 1 {
+		num = os.Args[1]
+	}
+
+	// AddOne
+	r, err := c.AddOne(context.Background(), &pb.NumberRequest{Number: num})
+	if err != nil {
+		log.Fatalf("Could not AddOne: %v", err)
+	}
+	log.Printf("AddOne: %d", r.Message)
+
+	// Square
+	r, err = c.Square(context.Background(), &pb.NumberRequest{Number: num})
+	if err != nil {
+		log.Fatalf("Could not Square: %v", err)
+	}
+	log.Printf("Square: %d", r.Message)
+}
